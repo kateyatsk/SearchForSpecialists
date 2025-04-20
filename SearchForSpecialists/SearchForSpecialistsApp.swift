73680ceac94cf40e89a54b06905a71e25c7a2882
kateyatsk <klaettoe2023@gmail.com>
2025-04-20 15:54:03 +0300

    Initial Commit

---
+++ SearchForSpecialists/SearchForSpecialistsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchForSpecialistsApp.swift
+//  SearchForSpecialists
+//
+//  Created by Екатерина Яцкевич on 20.04.25.
+//
+
+import SwiftUI
+
+@main
+struct SearchForSpecialistsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
